2c21cfbd380027de7b2750a862a986c042416326
Ice-cream1201 <54315272+Ice-cream1201@users.noreply.github.com>
2019-12-10 11:08:58 +0800

    11

--- meilele/src/App.js
+++ meilele/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch,Redirect} from 'react-router-dom'
 import 'antd/dist/antd.css'
 import Search from "./pages/search/Search"
 import Cart from "./pages/cart/Cart"
+import Home from './pages/home'
 
 class App extends Component {
     render() {
         return (
           <Router>
             <Switch>
+              <Route path="/home" exact component = {Home}/>
               <Route path="/search" exact component = {Search}/>
               <Route path="/cart" exact component = {Cart}/>
-              {/* <Redirect to = "/login" from = "/" exact/> */}
+              <Redirect to = "/home" from = "/" exact/>
               {/* <Route path="/" component = {NotFound}/> */}
             </Switch>
           </Router>
