f56fb91992e9d9ce0605ab95a0280c71135af51f
sxcr95 <359496700+sxcr95@users.noreply.github.com>
2019-12-12 17:05:35 +0800

    1111

--- meilele/src/pages/home/index.jsx
+++ meilele/src/pages/home/index.jsx
@@ -7,7 +7,6 @@ import List from './component/List'
 import Limit from './component/Limit'
 import NewStart from './component/NewStart'
 import Room from './component/Room'
-import React, { Component } from 'react'
 import PublicFooter from "../component/PublicFooter"
 
 export default class Home extends Component {
