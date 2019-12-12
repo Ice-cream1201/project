5401869ab57629874903dd7590d5b9a46f305b8f
sxcr95 <359496700+sxcr95@users.noreply.github.com>
2019-12-12 15:20:03 +0800

    1539

--- meilele/src/pages/classification/components/ClassHeader.jsx
+++ meilele/src/pages/classification/components/ClassHeader.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
+import {withRouter} from "react-router-dom"
 import ClassImg from "../../../assets/img/search1.png"
 import "../../../assets/css/classification/classHeader.css"
-export default class ClassHeader extends Component {
+class ClassHeader extends Component {
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick(){
+    this.props.history.push("/search")
+  }
   render() {
     return (
       <div className="classHeader">
-        <p className="classText">
+        <p className="classText" onClick={this.handleClick}>
           <img src={ClassImg} className="classImg" alt=""/>
           搜索商品</p>
       </div>
     )
   }
 }
+export default withRouter(ClassHeader)
--- meilele/src/pages/search/component/SearchHeader.jsx
+++ meilele/src/pages/search/component/SearchHeader.jsx
@@ -4,11 +4,18 @@ import "../../../assets/css/search/searchHeader.css"
 
 const { Search } = Input
 export default class SearchHeader extends Component {
+  constructor(props){
+    super(props)
+    this.searchClick = this.searchClick.bind(this)
+  }
+  searchClick(){
+    window.history.back(-1)
+  }
   render() {
     return (
       <div style={{width:"100%"}}>
         <Search placeholder="搜索商品" onSearch={value => console.log(value)} enterButton />
-        <span className="annuler">取消</span>
+        <span className="annuler" onClick={this.searchClick}>取消</span>
       </div>
     )
   }
